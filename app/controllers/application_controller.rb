3c0bd1a4bb53cd2b2028d99a7fc121bc23e0758a
lanzhiheng <hengrj@126.com>
2016-03-01 13:55:49 +0800

    #11 博文显示页面优化，专题下拉选择 1. 支持markdown显示。 2. 专题栏提供下拉选择。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
+# coding: utf-8
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   layout "custom"
+
+  # 每个页面几乎都会加载的数据，在这里获取。
+  def get_classifications
+    @classifications = Classification.all
+  end
+
+  def get_specials
+    @specials = Special.all
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
+# coding: utf-8
+require 'redcarpet'
 class ArticlesController < ApplicationController
+
+  before_filter :get_classifications, :get_specials
+
   def new
 
   end
@@ -14,12 +19,26 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-    @classifications = Classification.all
+    # 保证html安全，才能正确传送到view
+    @article_content = markdown.render(@article.body).html_safe
   end
 
+  def show_articles_by_classification
+    @articles = Article.where(classification_id: params[:id]).paginate(page: params[:page],per_page: 20).order("id DESC")
+    render 'home/index'
+  end
+
+  def show_articles_by_special
+    @articles = Article.where(special_id: params[:id]).paginate(page: params[:page],per_page: 20).order("id DESC")
+    render 'home/index'
+  end  
 
   private
   def article_params
     params.require(:article).permit(:title, :subtitle, :classification_id, :body)
+  end
+
+  def markdown
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
   end
 end
