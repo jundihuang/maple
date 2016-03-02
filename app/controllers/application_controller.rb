c7182f5195594c73bf5a4d5a8fee1186ace5e353
hellojundy <hellojundy@163.com>
2016-03-02 10:04:05 +0000

    界面优化   * 标签栏样式修改   * 分类样式修改   * banner样式修改   * css->scss代码风格

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,27 +19,27 @@ class ApplicationController < ActionController::Base
   def get_all_used_tags
     @tags = ActsAsTaggableOn::Tag.all
     #  根据@tags的长度产生对应的随机数
-    @word_size_list = rand_number(@tags.size, 8, 20)
+    #@word_size_list = rand_number(@tags.size, 8, 20)
     @word_color_list = rand_color(@tags.size)
   end
 
-
   private
+    
   def rand_number(n, min, max)
-    randoms = Set.new
-    loop do
-      randoms << min + rand(max - min)
-      return randoms.to_a if randoms.size >= n
+    randoms = Array.new
+    n.times do
+       randoms << rand(min..max)
     end
+    randoms
   end
 
   # 随机生成颜色
   def rand_color(n)
-    randoms = Set.new
-    loop do
+    randoms = Array.new
+    n.times do
       # 定死了前面8位就不会生成与背景色ffffff相近的颜色了，提高对比度
       randoms << "#"+ "00" +rand(16..255).to_s(16)+rand(16..255).to_s(16)
-      return randoms.to_a if randoms.size >= n
     end
+    randoms
   end
 end
