8523a59e4e654794b426cde28c65f65e56f4ad14
lanzhiheng <hengrj@126.com>
2016-08-13 18:34:28 +0800

    #11 顶部导航栏修改 - 改成固定到顶部。 - 滚动到下面的时候逐渐消失。 - 显示另一种风格的导航栏。

--- app/assets/javascripts/js/navbar/navbar.js
+++ app/assets/javascripts/js/navbar/navbar.js
@@ -1,6 +1,23 @@
-/* $(document).ready(function(){
-   $(".navbar-nav li").click(function(){
-   $(".navbar-nav .active").removeClass('active');
-   $(this).addClass('active');
-   });
-   }); */
+$(document).ready(function(){
+  $('.navbar-normal').click(function () {
+    console.log('good');
+  });
+  var oldClassName = navbar.className;
+  $(window).scroll(function (event) {
+    var banner = document.getElementById('banner');
+    var navbar = document.getElementById('navbar')
+    var bannerHeight = banner.offsetHeight;
+    var scrollHeight = window.scrollY;
+    // 定义透明度
+    if (scrollHeight + 400 >= bannerHeight && scrollHeight < bannerHeight) {
+      navbar.className = oldClassName;
+      navbar.style.opacity = (bannerHeight - scrollHeight) / 400;
+    } else if (scrollHeight > bannerHeight) {
+      navbar.style.opacity = 1;
+      navbar.className = oldClassName + " navbar-display";
+    } else {
+      navbar.style.opacity = 1;
+      navbar.className = oldClassName;
+    }
+});
+});
